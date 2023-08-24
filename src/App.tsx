84fc792dd5c2aa1fbc30b8cbffa8a307185870b8
Danilo Martinelli <danilomartinelli429@gmail.com>
2023-08-24 18:38:20 -0300

    feat: create table

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-// Definindo os tipos de dados
+import { Line } from 'react-chartjs-2';
 
 interface IData {
   time: number;
   LDRValue: number;
   light: boolean;
   light2: boolean;
+  timestamp: number;
 }
 
 const App: React.FC = () => {
@@ -17,46 +17,58 @@ const App: React.FC = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('https://smartlight-api.onrender.com/data');
-
-      // Ordena os registros pelo atributo time
-      const sortedData = response.data.sort((a: IData, b: IData) => b.time - a.time);
-
-      // Pega os últimos 10 registros
-      const lastTenRecords = sortedData.slice(0, 10);
-
-      setData(lastTenRecords);
+      const response = await axios.get('https://smartlight-api.onrender.com/data', {
+        params: {
+          limit: 10
+        }
+      });
+      setData(response.data);
       setLoading(false);
+    }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (err: any) {
+    catch (err: any) {
       setError(err.message);
       setLoading(false);
     }
   }
 
   useEffect(() => {
-    fetchData(); // Fazendo a chamada inicial
-    const interval = setInterval(fetchData, 5000); // Fazendo a chamada a cada 5 segundos
-
-    return () => clearInterval(interval); // Limpar o intervalo quando o componente for desmontado
+    fetchData();
+    const interval = setInterval(fetchData, 5000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div>
       <h1>Dados da API:</h1>
-      <button onClick={fetchData}>Refresh</button>
-      {loading ? <div>Carregando...</div> :
-        error ? <div>Erro: {error}</div> :
-          <ul>
-            {data.map((item, index) => (
-              <li key={index}>
-                <p>Tempo: {item.time}</p>
-                <p>Valor LDR: {item.LDRValue}</p>
-                <p>Luz: {item.light ? 'Ligada' : 'Desligada'}</p>
-                <p>Luz 2: {item.light2 ? 'Ligada' : 'Desligada'}</p>
-              </li>
-            ))}
-          </ul>}
+      {loading && !error && <p>Carregando...</p>}
+      {error && <p>{error}</p>}
+      {
+        !loading && !error && data.length > 0 && (
+          <table>
+            <thead>
+              <tr>
+                <th>time</th>
+                <th>LDRValue</th>
+                <th>light</th>
+                <th>light2</th>
+                <th>timestamp</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.time}</td>
+                  <td>{item.LDRValue}</td>
+                  <td>{item.light ? 'true' : 'false'}</td>
+                  <td>{item.light2 ? 'true' : 'false'}</td>
+                  <td>{item.timestamp}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )
+      }
     </div>
   );
 }
